69eb28322b9a084d36205c9e4e92b4dc92b21a40
Travis Hammonds <travishammonds2@gmail.com>
2023-11-15 11:55:14 -0500

    dfs

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -27,15 +27,130 @@ class BinarySearchTree {
     }
   }
 
-  find(key){
-    if(this.key === key){
+  find(key) {
+    if (this.key === key) {
       return this.value;
-    } else if (key < this.key && this.left){
+    } else if (key < this.key && this.left) {
       return this.left.find(key);
-    } else if (key > this.key && this.right){
+    } else if (key > this.key && this.right) {
       return this.right.find(key);
     } else {
       throw new Error("key not found");
     }
+  }
+
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      } else if (this.left) {
+        /* If the node only has a left child,
+         then you replace the node with its left child. */
+        this._replaceWith(this.left);
+      } else if (this.right) {
+        /* And similarly, if the node only has a right child,
+         then you replace it with its right child. */
+        this._replaceWith(this.right);
+      } else {
+        /* If the node has no children, then
+         simply remove it and any references to it
+         by calling `this._replaceWith(null)`. */
+        this._replaceWith(null);
+      }
+    } //}
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    } else if (key > this.key && this.right) {
+      this.right.remove(key);
+    } else {
+      throw new Error("Key Not Found");
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      } else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    } else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      } else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+
+  dfsInOrder(values = []) {
+    // First, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsInOrder(values);
+    }
+
+    // Next, process the current node
+    values.push(this.value);
+
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsInOrder(values);
+    }
+
+    return values;
+  }
+
+  dfsPreOrder(values=[]) {
+    // First, process the current node
+    values.push(this.value);
+
+    // Next, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsPreOrder(values);
+    }
+
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsPreOrder(values);
+    }
+
+    return values;
+  }
+
+  dfsPostOrder(values = []) {
+    // First, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsPostOrder(values);
+    }
+
+    // Next, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsPostOrder(values);
+    }
+
+    // Finally, process the current node
+    values.push(this.value);
+
+    return values;
   }
 }
